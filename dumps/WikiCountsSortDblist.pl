6e48acdde1bc4775634ad2b612db9859d54a66de
Erik Zachte <ezachte@users.mediawiki.org>
2012-06-22 20:49:33 +0000

    sort fix

--- dumps/WikiCountsSortDblist.pl
+++ dumps/WikiCountsSortDblist.pl
@@ -12,10 +12,15 @@ use Getopt::Std ;
   $file_dblist = @options {"d"} ;
   $suffix      = @options {"s"} ;
 
+# local test only:
+# $file_csv    = "dblists/StatisticsLog.csv" ;
+# $file_dblist = "dblists/wikinews.dblist" ;
+# $suffix      = "wikinews" ;
+
   if (! -e $file_csv)    { die "csv file '$file_csv' not found" ; }
   if (! -e $file_dblist) { die "dblist file '$file_dblist' not found" ; }
 
-  open CSV,    '<', $file_csv ;
+  print "\n\nSort dblist $file_dblist\nProcessing last dump took x seconds:\n\n" ;
   open DBLIST, '<', $file_dblist ;
   @dblist = <DBLIST> ;
   foreach $db (@dblist)
@@ -25,38 +30,52 @@ use Getopt::Std ;
   }
   close DBLIST ;
 
+  open CSV, '<', $file_csv ;
   while ($line = <CSV>)
   {
     ($lang,$dummy1,$dummy2,$runtime) = split (',', $line) ;
     $runtime =~ s/\&\#44;//g ;
-    if ($runtime =~ /hrs.*min.*sec/)
+
+    $runtime_dhms = $runtime ;
+    $runtime_dhms =~ s/^\s*0 days 0 hrs 0 min// ;
+    $runtime_dhms =~ s/^\s*0 days 0 hrs// ;
+    $runtime_dhms =~ s/^\s*0 days// ;
+    $runtime_dhms =~ s/^\s*// ;
+    $runtimes_dhms {"$lang$suffix"} = $runtime_dhms ;
+
+    if ($runtime =~ /days.*hrs.*min.*sec/)
+    { $runtime =~ s/^(\d+) days (\d+) hrs (\d+) min (\d+) sec.*$/$1*3600*234+$2*3600+$3*60+$4/e ; }
+    elsif ($runtime =~ /hrs.*min.*sec/)
     { $runtime =~ s/^(\d+) hrs (\d+) min (\d+) sec.*$/$1*3600+$2*60+$3/e ; }
     elsif ($runtime =~ /min.*sec/)
     { $runtime =~ s/^(\d+) min (\d+).*$/$1*60+$2/e ; }
     elsif ($runtime =~ /hrs.*min/)
     { $runtime =~ s/^(\d+) hrs (\d+).*$/$1*3600+$2*60/e ; }
-    @runtimes {$lang} = $runtime ;
+    $runtimes {"$lang$suffix"} = $runtime ;
   }
+  close CSV ;
+
   foreach $lang (sort {$runtimes {$b} <=> $runtimes {$a}} keys %runtimes)
   {
     # print "$lang$suffix\n" ;
-    @index {"$lang$suffix"} = ++$index ;
+    @wiki_rank {$lang} = ++$index ;
   }
 
-  @dblist = sort {@index {$b} <=> @index {$a}} @dblist ;
+  @dblist = sort {@wiki_rank {$b} <=> @wiki_rank {$a}} @dblist ;
 
   rename $file_dblist, $file_dblist.".bak" ;
 
+  $lines = 0 ;
   open DBLIST, '>', $file_dblist ;
   foreach $db (@dblist)
   {
-    $index = $index {$db} ;
-    print "$db: $index\n" ;
+    $rank = $wiki_rank {$db} ;
+    print (sprintf ("%3d", ++$lines) . ": $db - " . $runtimes {$db} . " - (" . $runtimes_dhms {$db} . ")\n") ;
     print DBLIST "$db\n" ;
   }
   close DBLIST ;
 
-  print "Ready" ;
+  print "\n==\n" ;
   exit ;
 
 
