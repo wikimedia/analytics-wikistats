55f2adf26e02e1d416728e0c8d5bf8042d2e8a87
Erik Zachte <ezachte@wikimedia.org>
2012-10-12 07:13:58 +0000

    Commit changed files, to prep for migration from /a/squids to /a/wikistat_git/squids

--- squids/SquidReportArchiveConfig.pm
+++ squids/SquidReportArchiveConfig.pm
@@ -1,20 +1,20 @@
 #!/usr/bin/perl
 
-  $cfg_liblocation       = "/a/squid/stats/scripts" ;
+  $cfg_liblocation       = "/a/wikistats_git/squids/" ;
 
-  $cfg_path_csv          = "/a/squid/stats/csv" ;
-  $cfg_path_reports      = "/a/squid/stats/reports" ;
-  $cfg_path_log          = "/a/squid/stats/scripts" ;
+  $cfg_path_csv          = "/a/squid/stats_editors/csv" ;
+  $cfg_path_reports      = "/a/squid/stats_editors/reports" ;
+  $cfg_path_log          = "/a/squid/stats_editors/scripts" ;
 
-  $cfg_path_csv_test     = "W:/# Out Locke" ;      # Erik
-  $cfg_path_reports_test = "W:/# Out Test/Locke" ; # Erik
-  $cfg_path_log_test     = "W:/# Out Test/Locke" ; # Erik
+#  $cfg_path_csv_test     = "W:/# Out Locke" ;      # Erik
+#  $cfg_path_reports_test = "W:/# Out Test/Locke" ; # Erik
+#  $cfg_path_log_test     = "W:/# Out Test/Locke" ; # Erik
 # $cfg_path_csv_test     = "/srv/erik/" ;          # André
 # $cfg_path_reports_test = "/srv/erik/" ;          # André
-  $cfg_path_log_test     = "/srv/erik/" ;          # André
+#  $cfg_path_log_test     = "/srv/erik/" ;          # André
 
 # set default arguments for test on local machine
-  $cfg_default_argv = "-m 2011-08" ;   # monthly report
+#  $cfg_default_argv = "-m 2011-08" ;   # monthly report
 # $cfg_default_argv = "-w" ;           # refresh country info from Wikipedia (population etc)
 # $cfg_default_argv = "-c" ;           # country/regional reports
 # $cfg_default_argv = "-c -q 2011Q4" ; # country/regional reports based on data for one quarter only
