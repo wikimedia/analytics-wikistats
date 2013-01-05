fc7840bb28e7bafea79a5ff0becc9e2186d98fab
Stefan Petrea <stefan@garage-coding.com>
2013-01-05 04:50:03 +0200

    added comments. separated test.pl from pageviews.pl. really need to write some tests

---
+++ pageviews_reports/test.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use lib "./lib";
+use lib "../squids/t/";
+use PageViews::Model;
+use PageViews::View;
+use PageViews::Generate1;
+use Generate::Squid;
+use CommonConfig;
+use Data::Dumper;
+use Carp;
+
+our $__CODE_BASE;
+our $__DATA_BASE        = "$__CODE_BASE/../pageviews_reports/data";
+my  $LOG_PREFIX         = "sampled-1000.log-";
+my  $REPORT_OUTPUT_PATH = "/tmp/pageview_reports/";
+
+`mkdir -p $REPORT_OUTPUT_PATH`;
+
+# overall_count_delta is the percentage by which counts increased/decreased
+# over the previous month
+my $config = [
+  {
+    month => "2012-10",
+    explicit_country_counts => {
+      us => 400,
+      gb => 300 ,
+      ca => 100 ,
+      fr => 100 ,
+      de => 30 ,
+    },
+  },
+  {
+    month => "2012-11",
+    overall_count_delta => 0.24,
+  },
+  {
+    month => "2012-12",
+    overall_count_delta => 0.30,
+  },
+  {
+    month => "2013-01",
+    overall_count_delta => -0.1,
+  },
+  {
+    month => "2013-02",
+    explicit_country_deltas => {
+      us => -0.3 ,
+      gb => +0.1 ,
+      de => +0.2 ,
+      fr => -0.02,
+      ca => +0.031415,
+    },
+  },
+];
+
+my $g = PageViews::Generate1->new({
+      config     => $config,
+    __DATA_BASE  => $__DATA_BASE,
+      LOG_PREFIX => $LOG_PREFIX,
+});
+$g->generate();
+
+
+my $m = PageViews::Model->new();
+$m->process_files({
+    logs_path => $__DATA_BASE,
+});
+
+#$Template::Directive::WHILE_MAX = 9999999;
+
+my $d = $m->get_data();
+#warn "[DBG]".Dumper($d);
+
+my $v = PageViews::View->new($d);
+
+#warn "[DBG] outpath => $REPORT_OUTPUT_PATH";
+$v->render({ output_path => $REPORT_OUTPUT_PATH });
