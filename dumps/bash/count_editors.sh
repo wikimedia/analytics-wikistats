8a9f3859cca90c345aabfa715b1243994610050b
Erik Zachte <ezachte@wikimedia.org>
2013-03-28 18:15:41 +0000

    added wikivoyage and draft mode
    Change-Id: Ib950f64c319cc2e5e20049e91629886ca9b7d764

--- dumps/bash/count_editors.sh
+++ dumps/bash/count_editors.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ulimit -v 8000000
 
 wikistats=/a/wikistats_git
@@ -6,15 +6,26 @@ dumps=$wikistats/dumps
 perl=$dumps/perl
 csv=$dumps/csv
 out=$dumps/out
+htdocs=stat1001.wikimedia.org::a/srv/stats.wikimedia.org/htdocs/
 
-clear
+clear 
+
+# only when run from ../bash folder in home folder, choose accompanying perl folder
+# cd ../perl is tricky, when running bash from anywhere else)  
+bashpath="${PWD}"
+# echo $bashpath
+if [[ "$bashpath" == *ezachte* ]] ; then
+  perl=/home/ezachte/wikistats/dumps/perl # tests
+  echo run from test perl folder "$perl"
+fi
+
 # -y = collect editor counts for one project
 # -z = collect editor counts for all projects
 # reporting step generates 
 
 cd $perl
 
-for w in wb wk wn wp wq ws wv wx 
+for w in wb wk wn wo wp wq ws wv wx 
 do 
   perl WikiCounts.pl -i $csv/csv_$w -o $csv/csv_$w/ -y ;
 done;
@@ -22,4 +33,11 @@ done;
 perl WikiCounts.pl -i $csv/csv_wp -o $csv/csv_wp/ -z ;
 
 perl WikiReports.pl -m wm -l en -i $csv/csv_wp -o $out/out_wp
+
+## publish renamed version:
+#  rsync -av $out/out_wp/EN/TablesWikimediaAllProjects.htm $htdocs/EN
+
+## publish draft version:
+rsync -av $out/out_wp/EN/TablesWikimediaAllProjects.htm $htdocs/EN/draft
+
 
