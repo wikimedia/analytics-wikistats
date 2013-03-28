654e974cd3f13d3086eb362723cabb15e4003abb
Erik Zachte <ezachte@wikimedia.org>
2013-03-28 18:13:51 +0000

    added draft mode
    Change-Id: I8e325aacbdb0b6b7f03cec11de988acc2310c649

--- dumps/bash/report_all_editors.sh
+++ dumps/bash/report_all_editors.sh
@@ -3,11 +3,21 @@
 wikistats=/a/wikistats_git
 dumps=$wikistats/dumps
 perl=$dumps/perl
+# perl=/home/ezachte/wikistats/dumps/perl # tests
 csv=$dumps/csv
 out=$dumps/out
 htdocs=stat1001.wikimedia.org::a/srv/stats.wikimedia.org/htdocs/
 
 cd $perl
 perl WikiReports.pl -m wm -l en -i $csv/csv_wp/ -o $out/out_wp
-rsync -av $out/out_wp/EN/TablesWikimediaAllProjects* $htdocs/EN
+
+## publish renamed version:
+#  rsync -av $out/out_wp/EN/TablesWikimediaAllProjects.htm $htdocs/EN
+
+## publish draft version:
+   rsync -av $out/out_wp/EN/TablesWikimediaAllProjects.htm $htdocs/EN/draft
+
+## publish renamed version:
+#  mv $out/out_wp/EN/TablesWikimediaAllProjects.htm $out/out_wp/EN/TablesWikimediaAllProjectsNoWikivoyage.htm
+#  rsync -av $out/out_wp/EN/TablesWikimediaAllProjectsNoWikivoyage.htm $htdocs/EN/draft
 
