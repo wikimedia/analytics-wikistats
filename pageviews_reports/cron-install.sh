68d7747b35f5859d0fa4baf30cdf1397b4383ef3
Petrea Corneliu Stefan <stefan@garage-coding.com>
2013-04-24 06:38:07 +0300

    changed time of cron job to 1st of each month at 07:20 stat1's time
    Change-Id: Ia964516b09b09fdc4b918b721058a7cdfdd69bbd

--- pageviews_reports/cron-install.sh
+++ pageviews_reports/cron-install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-crontab  -l | { cat; echo  "31 6 24 * * user . /home/user/.bashrc; /bin/bash /home/user/wikistats/wikistats/pageviews_reports/cron-script.sh"; } | crontab -
+# Since logs are written to disk on stat1's time at around 06:30
+# we place this job at 07:20 to be sure the needed files are in place
+crontab  -l | { cat; echo  "20 7 01 * * user . /home/user/.bashrc; /bin/bash /home/user/wikistats/wikistats/pageviews_reports/cron-script.sh"; } | crontab -
