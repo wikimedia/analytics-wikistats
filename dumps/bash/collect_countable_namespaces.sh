4ce2e67713aa3115e91c216f783b893763f45117
Erik Zachte <ezachte@wikimedia.org>
2013-08-12 17:06:34 +0000

    added rsync to web site
    Change-Id: I43eb1c8817086830244b3b2022deb218c721271f

--- dumps/bash/collect_countable_namespaces.sh
+++ dumps/bash/collect_countable_namespaces.sh
@@ -6,6 +6,7 @@ wikistats=/a/wikistats_git
 perl=$wikistats/dumps/perl
 perl=/home/ezachte/wikistats/dumps/perl # tests
 csv=$wikistats/dumps/csv
+htdocs=stat1001.wikimedia.org::a/srv/stats.wikimedia.org/htdocs/
 
 clear
 
@@ -21,5 +22,7 @@ grep "0|"      StatisticsContentNamespaces.csv >> StatisticsContentNamespacesExt
 
 date >> StatisticsContentNamespacesExtraNamespacesDiff.csv
 diff StatisticsContentNamespacesExtraNamespaces.csv StatisticsContentNamespacesExtraNamespaces.bak >> StatisticsContentNamespacesExtraNamespacesDiff.csv
+
+rsync -av StatisticsContentNamespacesExtraNamespaces.csv $htdocs/wikimedia/misc
 
 
