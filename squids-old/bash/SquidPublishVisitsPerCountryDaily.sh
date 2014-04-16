3820977805bc49b45ff6665b7d04a47b98f92351
Erik Zachte <ezachte@wikimedia.org>
2014-04-16 15:42:46 +0000

    dataset2 -> dataset 1001
    Change-Id: Ib63c5e386b82c807a54b90ac095b005b0f51c9c6

--- squids-old/bash/SquidPublishVisitsPerCountryDaily.sh
+++ squids-old/bash/SquidPublishVisitsPerCountryDaily.sh
@@ -2,7 +2,7 @@ wikistats=/a/wikistats_git
 squids=$wikistats/squids
 csv=$squids/csv
 htdocs=stat1001.wikimedia.org::a/srv/stats.wikimedia.org/htdocs/
-archive=dataset2::pagecounts-ez
+archive=dataset1001::pagecounts-ez
 
 cd $csv
 zip csv_squids_daily_visits_per_country.zip  SquidDataVisitsPerCountryDaily.csv
