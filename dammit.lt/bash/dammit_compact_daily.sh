8e3c2aad0fac6f0d53a689aace97dd43e1f16515
Erik Zachte <ezachte@wikimedia.org>
2014-06-07 17:32:05 +0000

    invocation of dammit_compact_monthly.sh fixed
    Change-Id: Ia2581d975f52cabcf71f971a0fd9e4d1a3d7b1c7

--- dammit.lt/bash/dammit_compact_daily.sh
+++ dammit.lt/bash/dammit_compact_daily.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 ulimit -v 2000000
 
-c1()(set -o pipefail;"$@" | perl -pe 's/.*/\e[1;32m$&\e[0m/g') # colorize output green
-c2()(set -o pipefail;"$@" | perl -pe 's/.*/\e[1;33m$&\e[0m/g') # colorize output yellow
+# c1()(set -o pipefail;"$@" | perl -pe 's/.*/\e[1;32m$&\e[0m/g') # colorize output green
+# c2()(set -o pipefail;"$@" | perl -pe 's/.*/\e[1;33m$&\e[0m/g') # colorize output yellow
+echo_() { 
+echo "$1" | tee -a $logfile| cat 
+}
 
 wikistats=/a/wikistats_git
 dammit=$wikistats/dammit.lt
@@ -20,31 +23,28 @@ yyyymmdd=$(date +"%Y_%m_%d")
 logfile=$logs/compact_daily_$yyyymmdd.log 
 logfile_summary=$logs/_summary_compact_daily_jobs.log 
 
+# extra step, in case yesterdays job did not finish normally
+date | tee -a $logfile | cat
+echo_ "rsync -arv --include=*.bz2 $output/* $dataset1001"
+# -a archive mode, -r recursive, -v verbose, -O do not try to upd dir timestamp
+rsync -arv --include=*.bz2 $output/* $dataset1001 | tee -a $logfile | cat
+
 cd $perl
 
-# mode=-m # specify -m for monthly combine of daily files, comment line for generating daily files
 maxage=14 # process files for last .. completed days (runs daily, so should have one day of work to do)
 
-echo Consolidate pagecount files into one daily file for last $maxage completed days 
-echo
-# c1 perl DammitCompactHourlyOrDailyPageCountFiles.pl $mode -a $maxage -i $input -o $output -t $temp | tee -a $logfile | cat
+echo_ "Consolidate pagecount files into one daily file for last $maxage completed days\n"
 # date > $bash/dsh/dammit_compact_daily.semaphoreammit_compact_daily.semaphore
 # flock -n = non block lock
 cmd="nice perl DammitCompactHourlyOrDailyPageCountFiles.pl $mode -a $maxage -i $input -o $output -t $temp | tee -a $logfile | cat"
 flock -n -e $bash/dammit_compact_daily.semaphore -c "$cmd" || { echo "Script is already running: lock on ../bash/dammit_compact_daily.semaphore" ; exit 1 ; } >&2
 
-echo Consolidate pagecount files for whole month into one monthly file - only finds work to do on first day of new month 
-echo
+echo_ "Consolidate pagecount files for whole month into one monthly file - only finds work to do on first day of new month\n"
 # c1 $bash/dammit_compact_monthly.sh 
-flock -n -e $bash/dammit_compact_monthly.semaphore -c "Run dammit_compact_daily.sh" || { echo "Script dammit_compact_daily.sh is already running: lock on ../bash/dammit_compact_daily.semaphore" ; exit 1 ; } >&2
+flock -n -e $bash/dammit_compact_monthly.semaphore -c "$bash/dammit_compact_monthly.sh" || { echo "Script dammit_compact_monthly.sh is already running: lock on ../bash/dammit_compact_monthly.semaphore" ; exit 1 ; } >&2
 
-cd $bash
-./dammit_compact_monthly.sh 
-
-echo Publish new files 
-echo
-# c2 rsync -arv --include=*.bz2 $output/* $dataset1001
-     rsync -arv --include=*.bz2 $output/* $dataset1001
+echo_ "Publish new files\n"
+rsync -arv --include=*.bz2 $output/* $dataset1001  | tee -a $logfile | cat
 
 
 #grep ">>" $logs/*.log
