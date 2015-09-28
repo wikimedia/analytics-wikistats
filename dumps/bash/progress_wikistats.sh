af393c26fca28b1858677c5685f266faa3d81154
Erik Zachte <ezachte@wikimedia.org>
2015-09-28 12:04:14 +0000

    plus gomwiki
    Change-Id: I30616f45f16d5af1459fbb4c10e57631f6783d0e

--- dumps/bash/progress_wikistats.sh
+++ dumps/bash/progress_wikistats.sh
@@ -1,10 +1,12 @@
+
 #!/bin/sh
 ulimit -v 1000000
-
 wikistats=/a/wikistats_git
 dumps=$wikistats/dumps
+
 perl=$dumps/perl
 perl=/home/ezachte/wikistats/dumps/perl # tests
+
 out=$dumps/out
 dammit=/a/dammit.lt
 htdocs=stat1001.eqiad.wmnet::srv/stats.wikimedia.org/htdocs
@@ -13,5 +15,6 @@ cd $perl
 perl WikiCountsJobProgress.pl -i $dumps -d $dammit -o $out -u $wikistats -p $perl
 
 echo "rsync -av $out/out_wm/WikiCountsJobProgress*.html $htdocs"
+
 rsync -av $out/out_wm/WikiCountsJobProgress*.html $htdocs
 
