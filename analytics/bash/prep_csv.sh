c891b08776804d41b1d6db82e62be6456143c45a
Erik Zachte <ezachte@wikimedia.org>
2016-03-26 16:57:50 +0000

    paths + comments + run parms changed
    Change-Id: If63e18573f94ea537e36411c1353cc795a2e9838

--- analytics/bash/prep_csv.sh
+++ analytics/bash/prep_csv.sh
@@ -28,9 +28,9 @@ yyyymm2=$(date -d "2 month ago" +"%Y-%m")
 #yyyymm=2012-08 # !!!!!!  yyyymm "1 month ago" on 31 July gives 2012-07 !!!1 
 
 #hard coded until auto set is more robust (day 21-31 use previous month, day 1-20 use two months ago)
-yyyymm=2015-09 # last month to report on  
-yyyymm2=2015-08 # previous month for comto compare 
-yyyymm_rc=2015-11 # rc is month of RC meeting
+yyyymm=2016-02 # last month to report on  
+yyyymm2=2016-01 # previous month for comto compare 
+yyyymm_rc=2016-03 # rc is month of RC meeting
 
 echo process data up to $yyyymm and write to rc-$yyyymm.zip
 log=$analytics/logs/prep_csv_$yyyymmdd.log 
@@ -72,15 +72,15 @@ c1 perl AnalyticsPrepWikiCountsOutputMisc.pl -i $csv_dumps -o $csv_rc/$yyyymm -m
 c1 perl AnalyticsPrepWikiCountsOutputCore.pl -i $csv_dumps -o $csv_rc/$yyyymm -m $yyyymm $unmerged | tee -a $log | cat
 
 # analytics_in_page_views.csv is written daily as part of WikiCountsSummarizeProjectCounts.pl 
-# part of (/home/ezachte/pageviews_monthly.sh job) 
-# which processes hourly projectcounts files (per wiki page view totals for one hour) from http://dammit.lt/wikistats
+# part of (/home/ezachte/wikistats/projectviews_monthly.sh job) (monthly refers to output granularity)
+# which processes hourly projectviews files (per wiki page view totals for one hour) from https://dumps.wikimedia.org/other/pageviews/
 # and generates several files on different aggregation levels
 # only action here is to copy data to this folder to have everything in one place
 # note: unlike folder name suggests this file contains stats for all projects
+# for overview see https://wikitech.wikimedia.org/w/images/8/8d/Monthly_Pageview_Reports.png
 
-
-cp $csv_dumps/csv_wp/analytics_in_page_views.csv $csv_rc/$yyyymm # adjust to wikistat_git when dump process has been migrated
-cp $csv_dumps/csv_wp/wikilytics_in_pageviews.csv $csv_rc/$yyyymm # adjust to wikistat_git when dump process has been migrated
+cp /a/dammit.lt/projectviews/csv/csv_wp/analytics_in_page_views.csv $csv_rc/$yyyymm 
+cp /a/dammit.lt/projectviews/csv/csv_wp/wikilytics_in_pageviews.csv $csv_rc/$yyyymm 
 
 #echo compare csv files from folders $yyyymm2 and $yyyymm
 #c2 perl AnalyticsCompareMonthlyCsvFiles.pl -c $csv_rc -1 $yyyymm -2 $yyyymm2 -t 2 -f wikilytics_in_pageviews.csv 
