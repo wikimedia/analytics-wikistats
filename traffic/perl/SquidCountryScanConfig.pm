34dffbaa8d9c38b7044fca963fbf886bd51042f3
Erik Zachte <ezachte@wikimedia.org>
2016-10-10 13:57:34 +0000

    define env for SquidCountryScan.pl
    Change-Id: Ifb7173be325b9a431edc7f3e7c1c917a0f1103d5

---
+++ traffic/perl/SquidCountryScanConfig.pm
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+$wikistats         = "/a/wikistats_git/" ;
+$squids            = "$wikistats/squids/" ;
+
+$cfg_liblocation   = "$squids/perl/" ;
+$cfg_liblocation   = "/home/ezachte/wikistats/squids-scripts-2012-10/perl" ; # temp
+$cfg_path_csv      = "$squids/csv/" ;
+$cfg_path_csv_test = "w:/! perl/squids/archive/" ; # Erik
+# $cfg_path_csv_test = "?" ;                       # André
+
+$cfg_path_log      = "$squids/logs/" ;
+$cfg_path_log_test = "w:/! perl/squids/archive/" ; # Erik 
+# $cfg_path_log_test = "?" ;                       # André 
