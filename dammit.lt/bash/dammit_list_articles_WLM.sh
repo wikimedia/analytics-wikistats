be088ce8edda68d8cf42d8fc4c98a5999aa065c0
Erik Zachte <ezachte@wikimedia.org>
2016-10-22 00:31:29 +0000

    collect images for WLM 2016
    Change-Id: I06619f40c5f315d604d622c9e5b33bead8b6ba85

---
+++ dammit.lt/bash/dammit_list_articles_WLM.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Q&D hack to retrieve uploaders to Wiki Loves Monuments
+# use ./dammit_list_views_by_category.sh to get list of images within category 'Images_from_Wiki_Loves_Monuments_2016' 
+# use Q&D DammitScanPages.pl to collect username and url of image 
+wikistats=/a/wikistats_git
+dumps=$wikistats/dumps
+perl=$dumps/perl
+perl=/home/ezachte/wikistats/dammit.lt/perl # tests
+bash=$wikistats/dammit.lt/bash
+out=$dumps/out
+projectcounts=/a/dammit.lt/projectcounts
+projectviews=/a/dammit.lt/projectviews
+log=$projectviews/log_projectviews.txt
+csv_in=$dumps/csv
+csv_out=$projectviews/csv
+meta=$csv_in/csv_mw/MetaLanguages.csv
+date_switch="201609"
+htdocs=stat1001.eqiad.wmnet::srv/stats.wikimedia.org/htdocs/
+
+cd $bash
+# ./dammit_list_views_by_category.sh 'commons.wikimedia.org' 'Images_from_Wiki_Loves_Monuments_2016' 2016-09 1 'wm-commons' 2
+
+cd $perl
+perl DammitScanPages.pl
+
+cd /a/dammit.lt/pagecounts/categorized/data/wm-commons/2016-09/Images_from_Wiki_Loves_Monuments_2016
+rsync -arv -ipv4 WLM_uploaders.html stat1001.eqiad.wmnet::srv/stats.wikimedia.org/htdocs
