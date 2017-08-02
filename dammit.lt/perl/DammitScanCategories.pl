d65a1420479dd15135d3bdc85e741ce1e87a458c
Erik Zachte <ezachte@wikimedia.org>
2017-08-02 12:15:45 +0000

    upd proxy code
    Change-Id: Ib30b6f578385184239c8966470eb5dcbc17c3bdc

--- dammit.lt/perl/DammitScanCategories.pl
+++ dammit.lt/perl/DammitScanCategories.pl
@@ -378,7 +378,8 @@ sub GetPage
   my $url = URI::Heuristic::uf_urlstr($raw_url);
 
   my $ua = LWP::UserAgent->new();
-  $ua->proxy(["http"], $ENV{"http_proxy"}) ;
+# $ua->proxy(["http"], $ENV{"http_proxy"}) ;
+  $ua->proxy(["https"], $ENV{"https_proxy"}) ;
   $ua->agent("Wikimedia Perl job / EZ");
   $ua->timeout(60);
 
