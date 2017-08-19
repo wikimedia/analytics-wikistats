b3438f95b0be17c6c85946d09119b36e755e8b9c
Erik Zachte <ezachte@wikimedia.org>
2017-08-19 13:13:36 +0000

    cleanup
    Change-Id: I8c76ec1465911743b36bf001bf32a885c4a32e32

--- backup/backup_crontab.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-yyyymmdd=$(date +"%Y_%m_%d")
-
-crontab -l > /home/ezachte/crontab.backup
-crontab -l > /home/ezachte/wikistats_backup/crontab/crontab_backup_$yyyymmdd
--- dumps/bash/staff2opml.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd /home/ezachte/wikistats/dumps/perl
-# wget -O /home/ezachte/Staff_and_contractors.html http://wikimediafoundation.org/wiki/Staff_and_contractors
-
-perl staff2opml.pl -i /home/ezachte/Staff_and_contractors.html -o /home/ezachte/Staff_and_contractor.opml
-rsync -av /home/ezachte/Staff_and_contractor.opml thorium.eqiad.wmnet::srv/stats.wikimedia.org/htdocs/wikimedia
